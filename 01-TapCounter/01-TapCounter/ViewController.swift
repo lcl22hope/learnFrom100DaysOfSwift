408ce137fbcdd3ae1d5cba0e3739400de48aff05
lcl22hope <lcl22hope@gmail.com>
2016-08-16 16:06:23 +0800

    counter increase logic & long press increase count logic

--- 01-TapCounter/01-TapCounter/ViewController.swift
+++ 01-TapCounter/01-TapCounter/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // remove background semitransparent style and bottom line
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Outlets
+    @IBOutlet weak var countLabel: UILabel!
+    var count = 0
+    
+    // MARK: - Interactions
+    
+    @IBAction func tapButton(sender: AnyObject) {
+        increaseCount()
+    }
+    
+    @IBAction func tapResetButton(sender: AnyObject) {
+        count = 0
+        countLabel.text = "0"
+    }
+    
+    @IBAction func holdTapButton(sender: UILongPressGestureRecognizer) {
+        increaseCount()
     }
 
-
+    
+    func increaseCount() {
+        count += 1
+        countLabel.text = String(count)
+    }
 }
 
