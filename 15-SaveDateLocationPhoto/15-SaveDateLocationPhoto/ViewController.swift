f4c7c57259c3dd77973441e85db74beeb94499c4
lcl22hope <lcl22hope@gmail.com>
2016-08-24 15:50:24 +0800

    1.UI Basics

---
+++ 15-SaveDateLocationPhoto/15-SaveDateLocationPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  15-SaveDateLocationPhoto
+//
+//  Created by 玉皇大亮 on 8/24/16.
+//  Copyright © 2016 aaron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
