1da9730e95d143d0a554258d4a458e2c7e167e69
lcl22hope <lcl22hope@gmail.com>
2016-08-24 18:35:06 +0800

    before add coredata feature

--- 15-SaveDateLocationPhoto/15-SaveDateLocationPhoto/ViewController.swift
+++ 15-SaveDateLocationPhoto/15-SaveDateLocationPhoto/ViewController.swift
@@ -7,19 +7,91 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , CLLocationManagerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    // MARK: - Override
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newEntryTextView.becomeFirstResponder()
+        newEntryTextView.inputAccessoryView = newEntryInputAccessoryView
+        
+        locationManager.delegate = self
+    }
+    
+    // MARK: - Properties
+    let locationManager = CLLocationManager()
+    let dateFormatter = NSDateFormatter()
+
+    // MARK: - Outlets
+    @IBOutlet weak var newEntryDateLabel: UILabel!
+    @IBOutlet weak var newEntryLocationLabel: UILabel!
+    @IBOutlet weak var newEntryPhotoImageView: UIImageView!
+    @IBOutlet weak var newEntryTextView: UITextView!
+    @IBOutlet weak var newEntryInputAccessoryView: UIView!
+    
+    
+    // MARK: - Interactions
+    @IBAction func tapDoneButton(sender: UIBarButtonItem) {
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func tapCameraButton(sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func tapLocateButton(sender: UIButton) {
+        getLcoation()
     }
 
-
+    
+    // MARK: - delegate
+    // MARK: LocationDelegate
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(manager.location!) { (placemarks, error) in
+            if error != nil {
+                print("Error: \(error!.localizedDescription)")
+            }
+            else if placemarks?.count > 0 {
+                let firstPlaceMark = placemarks![0]
+                self.convertDataToWordsFromLocations(firstPlaceMark)
+            }
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error.localizedDescription)
+    }
+    
+    // MARK: ImagePickerDelegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        newEntryPhotoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismissViewControllerAnimated(true, completion: nil)
+        newEntryTextView.becomeFirstResponder()
+    }
+    
+    // MARK: - Functions
+    func convertDataToWordsFromLocations(placemark: CLPlacemark) {
+        locationManager.stopUpdatingLocation()
+        
+        let city    = placemark.locality!
+        let state   = placemark.administrativeArea!
+        let country = placemark.ISOcountryCode!
+        let zipCode = placemark.postalCode ?? ""
+        
+        let location = "\(city),\(state) \(zipCode), \(country)"
+        newEntryLocationLabel.text = location
+        newEntryLocationLabel.textColor = UIColor.blackColor()
+    }
+    
+    func getLcoation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
 }
 
