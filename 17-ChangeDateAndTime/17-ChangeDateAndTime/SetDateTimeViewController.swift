592c7c66b73baaa3a339cd0772c6d36c841dbf7a
lcl22hope <lcl22hope@gmail.com>
2016-08-25 16:01:20 +0800

    simple demo for DatePicker

---
+++ 17-ChangeDateAndTime/17-ChangeDateAndTime/SetDateTimeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SetDateTimeViewController.swift
+//  17-ChangeDateAndTime
+//
+//  Created by 玉皇大亮 on 8/25/16.
+//  Copyright © 2016 aaron. All rights reserved.
+//
+
+import UIKit
+
+class SetDateTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 17-ChangeDateAndTime/17-ChangeDateAndTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  17-ChangeDateAndTime
+//
+//  Created by 玉皇大亮 on 8/25/16.
+//  Copyright © 2016 aaron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
