88af35cfbfefbf24e4bd5da907117ea6e28d54a7
lcl22hope <lcl22hope@gmail.com>
2016-08-25 22:15:59 +0800

    1.learn viewController fixed space, 2.how to change backItem Image

---
+++ 18-CustomCollectionView/18-CustomCollectionView/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  18-CustomCollectionView
+//
+//  Created by 玉皇大亮 on 8/25/16.
+//  Copyright © 2016 aaron. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+}
