359b5c0712822537cfc973ea6b4f0d53271bfe4d
lcl22hope <lcl22hope@gmail.com>
2016-08-26 15:56:37 +0800

    searchController demo, caution:need to init searchViewController & display model need cast data

---
+++ 20-SearchBarInTableView/20-SearchBarInTableView/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  20-SearchBarInTableView
+//
+//  Created by 玉皇大亮 on 8/26/16.
+//  Copyright © 2016 aaron. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+}
